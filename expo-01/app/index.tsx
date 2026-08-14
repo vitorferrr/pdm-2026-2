547895f2e80549d38d8deebad4a4d71d1bf111c0
Vitor dos Santos Ferreira <vitorferr1412@gmail.com>
2026-08-14 23:04:41 +0000

    add: Criação dos components

--- expo-01/app/index.tsx
+++ expo-01/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Profile } from '../components/Profile';
 
 export default function Index() {
   return (
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Profile/>
+      <Text>Vitor Ferreira</Text>
     </View>
   );
 }
---
+++ expo-01/components/Profile.tsx
@@ -0,0 +1,11 @@
+import { Image, View } from 'react-native';
+
+export function Foto () {
+    return (
+        <View>
+            <Image
+                source={require}
+            />
+        </View>
+    )
+}
